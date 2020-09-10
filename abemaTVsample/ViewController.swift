580d8b00cce90d726e621839d165e5d4232da25d
ikemoti <58681457+ikemoti@users.noreply.github.com>
2020-09-10 18:16:34 +0900

    Initial Commit

---
+++ abemaTVsample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  abemaTVsample
+//
+//  Created by Sousuke Ikemoto on 2020/09/10.
+//  Copyright © 2020 Sousuke Ikemoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
